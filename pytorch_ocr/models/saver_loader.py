72dba89e02c7d8f83dcd7e4122afc06242a10f74
Nick To <nick@nickto.net>
2023-09-21 20:25:38 +0300

    Enforce types

--- pytorch_ocr/models/saver_loader.py
+++ pytorch_ocr/models/saver_loader.py
@@ -22,14 +22,14 @@ def save_model(model: CRNN, classes: List[str], path: str, overwrite: bool = Fal
     params_path = os.path.join(path, f"params.yaml")
     params = {}
     params["crnn"] = {
-        "resolution": model.resolution,
-        "dims": model.dims,
-        "num_chars": model.num_chars,
-        "use_attention": model.use_attention,
-        "use_ctc": model.use_ctc,
-        "grayscale": model.grayscale,
+        "resolution": [int(i) for i in model.resolution],
+        "dims": int(model.dims),
+        "num_chars": int(model.num_chars),
+        "use_attention": bool(model.use_attention),
+        "use_ctc": bool(model.use_ctc),
+        "grayscale": bool(model.grayscale),
     }
-    params["classes"] = classes
+    params["classes"] = [str(c) for c in classes]
     with open(params_path, "w") as f:
         yaml.safe_dump(params, f)
     print(f"saved model params to {params_path}")
